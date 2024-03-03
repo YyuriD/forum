a4ac8d50f0e0fc4ebdad0b7852263c381058f68f
YyuriD <yurid.fkg@gmail.com>
2024-03-03 18:32:06 +0200

    Added authentication by session ID

--- src/main/java/telran/java51/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java51/security/AuthorizationConfiguration.java
@@ -1,26 +1,30 @@
 package telran.java51.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
+import jakarta.websocket.Session;
+import lombok.RequiredArgsConstructor;
 import telran.java51.accounting.model.Role;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthorizationConfiguration {
-	@Autowired
-	CustomWebSecurity customWebSecurity;
+	
+	 final CustomWebSecurity customWebSecurity;
 
 	@Bean
 	public SecurityFilterChain web(HttpSecurity http) throws Exception {
 		http.httpBasic(Customizer.withDefaults());
 		http.csrf(csrf -> csrf.disable());
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));// add cookies and session authn
 		http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/account/register", "/forum/posts/**")
 				.permitAll().requestMatchers("/account/user/{login}/role/{role}").hasRole(Role.ADMINISTRATOR.name())
 				.requestMatchers(HttpMethod.PUT, "/account/user/{login}")
@@ -42,7 +46,7 @@ public class AuthorizationConfiguration {
 				})
 				.requestMatchers(HttpMethod.DELETE, "/forum/post/*").access((authentication, context) -> {
 					String[] arr = context.getRequest().getServletPath().split("/");
-					String postId = arr[arr.length - 1];
+					String postId = arr[arr.length - 1];		
 					return new AuthorizationDecision(
 							customWebSecurity.checkPostAuthor(postId, authentication.get().getName())
 									|| context.getRequest().isUserInRole(Role.MODERATOR.name()));
